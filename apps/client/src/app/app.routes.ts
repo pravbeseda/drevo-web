a4868f6a12e8354011190eb4a15919f7b9c152be
Alexander Ivanov <pravbeseda@yandex.ru>
2024-12-10 07:37:29 +0500

    Routing

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'article/edit',
+        loadComponent: () =>
+            import('./pages/article/article-edit/article-edit.component').then(
+                m => m.ArticleEditComponent
+            ),
+    },
+    {
+        path: '**',
+        redirectTo: 'article/edit',
+    },
+];
