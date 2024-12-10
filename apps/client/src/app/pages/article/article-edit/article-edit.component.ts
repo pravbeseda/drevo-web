6cf6e96f78b5258db0ef4dd2afa008339ae4f561
Alexander Ivanov <pravbeseda@yandex.ru>
2024-12-10 07:22:05 +0500

    Removed zone.js, prettifier setup. Added ArticleEditComponent

---
+++ apps/client/src/app/pages/article/article-edit/article-edit.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-edit',
+  imports: [],
+  templateUrl: './article-edit.component.html',
+  styleUrl: './article-edit.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ArticleEditComponent {
+
+}
