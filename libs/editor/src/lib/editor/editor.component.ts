15ec0af33bfd8b902d1612df67e95332f0cc1639
Alexander Ivanov <pravbeseda@yandex.ru>
2024-12-14 07:17:13 +0500

    codemirror works

--- libs/editor/src/lib/editor/editor.component.ts
+++ libs/editor/src/lib/editor/editor.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    Inject,
+    PLATFORM_ID,
+    ViewChild,
+} from '@angular/core';
+import { CommonModule, isPlatformServer } from '@angular/common';
+import { EditorView } from '@codemirror/view';
+import { EditorState } from '@codemirror/state';
+import { basicSetup } from 'codemirror';
 
 @Component({
     selector: 'lib-editor',
     imports: [CommonModule],
     templateUrl: './editor.component.html',
-    styleUrl: './editor.component.css',
+    styleUrl: './editor.component.scss',
 })
-export class EditorComponent {}
+export class EditorComponent implements AfterViewInit {
+    @ViewChild('editorContainer')
+    editorContainer?: ElementRef;
+
+    constructor(private host: ElementRef, @Inject(PLATFORM_ID) private platformId: object) {}
+
+    ngAfterViewInit(): void {
+        if (isPlatformServer(this.platformId)) {
+            return;
+        }
+
+        if (!this.editorContainer) {
+            return;
+        }
+
+        const editor = new EditorView({
+            state: EditorState.create({
+                doc: 'Text...',
+                extensions: [basicSetup],
+            }),
+            parent: this.editorContainer.nativeElement,
+        });
+    }
+}
