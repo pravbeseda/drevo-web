090ef6362582cac87c307166e0688de87e5fa842
Alexander Ivanov <pravbeseda@yandex.ru>
2025-04-08 07:20:19 +0500

    disable route tracing in app configuration

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -7,11 +7,13 @@ import { provideRouter, RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+const routesTracing = false;
+
 export const appConfig: ApplicationConfig = {
     providers: [
         provideClientHydration(withEventReplay()),
         provideExperimentalZonelessChangeDetection(),
         provideRouter(appRoutes),
-        importProvidersFrom(RouterModule.forRoot(appRoutes, { enableTracing: true })),
+        importProvidersFrom(RouterModule.forRoot(appRoutes, { enableTracing: routesTracing })),
     ],
 };
