bac7ef98b911f44c04633e6e61415ed9870a157b
Alexander Ivanov <pravbeseda@yandex.ru>
2025-12-29 11:44:25 +0400

    test(layout): add HttpClient and HttpClientTesting providers to LayoutComponent tests

--- apps/client/src/app/layout/layout.component.spec.ts
+++ apps/client/src/app/layout/layout.component.spec.ts
@@ -1,14 +1,19 @@
 import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 
 import { LayoutComponent } from './layout.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('LayoutComponent', () => {
-  let spectator: Spectator<LayoutComponent>;
-  const createComponent = createComponentFactory(LayoutComponent);
+    let spectator: Spectator<LayoutComponent>;
+    const createComponent = createComponentFactory({
+        component: LayoutComponent,
+        providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
 
-  it('should create', () => {
-    spectator = createComponent();
+    it('should create', () => {
+        spectator = createComponent();
 
-    expect(spectator.component).toBeTruthy();
-  });
+        expect(spectator.component).toBeTruthy();
+    });
 });
